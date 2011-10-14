295c2eef1ad2239efc420c0d159cf624b7c38f48
Net7x <net7x@pisem.net>
2011-10-14 12:05:33 +0600

    First

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Интернет магазин ВАМ ТОВАРЫ</title>
+  </head>
+  <body>
+    <?php
+    // put your code here
+    ?>
+  </body>
+</html>
